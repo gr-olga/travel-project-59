c84517f57b7502eb751dbebddbcb53d71b51cfa5
Olga Panfilova <gr-olga@ya.ru>
2022-08-17 11:13:42 +0200

    data adding for place page

--- src/components/PlacePage/PlacePage.js
+++ src/components/PlacePage/PlacePage.js
@@ -1,34 +1,53 @@
-import pic from "../../picture/488-4887502_plane-logos-travel-design-plane-logo-png-transparent.png"
 import "./PlacePage.css"
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function PlacePage() {
     const cities = [
         {id: 1, name: "Rome"},
         {id: 2, name: "Amsterdam"},
         {id: 3, name: "London"},
-        {id: 4, name: "Amsterdam"},
+        {id: 4, name: "Barcelona"},
     ]
+    const [list, setList] = useState()
+
+    async function getList() {
+        const res = await axios.get(
+            "http://localhost:4000/places"
+        );
+        setList(res.data)
+    }
+
+    useEffect(() => {
+        getList()
+        // setList(res.data);
+    }, []);
+
+    console.log("list", list);
     const [cityName, setCityName] = useState("")
     const changeSorting = event => {
         console.log("new sort order:", event.target.value);
         setCityName(event.target.value);
     };
+    const citiesSorted = cityName === '' ? list : list.filter((item) => item.name === cityName)
+    console.log("citiesSorted", citiesSorted);
+
     return (
         <div className="home-place-box">
             <section>
+                <span className="custom-dropdown big">
                 <select onChange={changeSorting}>
-                    {cities.length > 0
-                    && cities.map((item, i) => {
+                    {list && list.map((item, i) => {
                         return (
                             <option key={i} value={item.name}>{item.name}</option>
                         )
                     })}
                 </select>
+    </span>
             </section>
             <h3>{cityName}</h3>
-            <span>Description</span>
-            <img src={pic}/>
+            <span className="place-des">{citiesSorted[0].description}</span>
+            <img src={citiesSorted[0].image} alt="pic"/>
         </div>
     )
 }
