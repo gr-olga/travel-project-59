003c489150e80a48f346a5b245d2375d659309b0
AswadOsei <aswadoosterling@hotmail.com>
2022-08-18 16:31:11 +0200

    added visit button and css

--- src/components/Attraction/Attraction.jsx
+++ src/components/Attraction/Attraction.jsx
@@ -1,43 +1,58 @@
-import {useParams} from "react-router-dom";
-import "./Attraction.css"
+import { useParams, useNavigate } from "react-router-dom";
+import "./Attraction.css";
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function Attraction() {
+  const [attr, setAttr] = useState();
+  const params = useParams();
+  const navigate = useNavigate();
 
-    const [attr, setAttr] = useState()
-    const params = useParams();
+  console.log(params.city);
 
-    console.log(params.city);
+  async function getAttractions() {
+    const res = await axios.get(`http://localhost:4000/places/${params.city}`);
+    console.log("res", res);
+    return res;
+    // setList(res.data)
+  }
 
-    async function getAttractions() {
-        const res = await axios.get(
-            `http://localhost:4000/places/${params.city}`
-        );
-        console.log("res", res);
-        return res
-        // setList(res.data)
-    }
+  useEffect(() => {
+    getAttractions().then((r) => setAttr(r.data.placeDetails));
+  }, []);
 
-    useEffect(() => {
-        getAttractions().then(r => setAttr(r.data.placeDetails))
-    }, [])
-
-    return (
-        <div className="attract-box">
-            <p>hi</p>
-            <p>{params.city}</p>
-            <div className="cards-grid">
-                {attr && attr.map((item, index) => {
-                    return (
-                        <>
-                            <a href={`/${params.city}/${item.id}`}> <img className="card" src={item.image} key={index}/></a>
-                        </>
-                    )
-                })}
-            </div>
-        </div>
-    )
+  return (
+    <div className="attract-box">
+      {/* <p>hi</p> */}
+      <p>{params.city}</p>
+      <div className="cards-grid">
+        {attr &&
+          attr.map((item, index) => {
+            return (
+              <>
+                <a href={`/${params.city}/${item.id}`}>
+                  {" "}
+                  <img
+                    className="card"
+                    src={item.image}
+                    key={index}
+                    alt={item.name}
+                    style={{
+                      width: "300px",
+                      height: "200px",
+                      objectFit: "fill",
+                    }}
+                  />
+                  <button className="visit-btn" onClick={() => navigate(+1)}>
+                    {item.name}
+                  </button>
+                </a>
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
 //postgres://vlrdsado:G8SPV8DC2irijhUTp095DPZcKes9QgOv@abul.db.elephantsql.com/vlrdsado
